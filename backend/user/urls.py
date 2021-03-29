5343a498653b8cd652d4785a3427a5d71ac93d64
Dino Rossi <dinoprossi@gmail.com>
2021-03-29 20:08:03 +0200

    not much

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,13 +1,7 @@
-from django.urls import path
+# from django.urls import path
 
-# from user.views import
+# from user.views import GetAllUserView
 
 urlpatterns = [
     # path('', GetAllUserView.as_view()),
-    # path('me/', GetUserUpdateDeleteView.as_view()),
-    # path('<int:pk>/', GetSpecificUserView.as_view()),
-    # path('followers/following/', GetUserFollowingView.as_view()),
-    # path('followers/followers/', GetUserFollowerView.as_view()),
-    # path('followers/toggle-follow/<int:user_id>/', UserFollowUnFollowToggle.as_view()),
-    # path('?search=<str:search_string>/', UrlUserSearchView.as_view()),
 ]
