4f68448d86c4c1faee538d9dab5d9fb75866f061
Andras Gombos <andrisgombos@gmail.com>
2021-03-29 16:31:40 +0200

    added routes to app.js

--- frontend/luna_project/src/components/user/index.jsx
+++ frontend/luna_project/src/components/user/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Title from '../../assets/pictures/userFace.JPG'
 
-export default function profilePage() {
+export default function ProfilePage() {
     return (
         <>
         <TitleImage>
