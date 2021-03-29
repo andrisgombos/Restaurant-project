6f7a40720f809da30ad49a8e89d1fb2a1b0fc844
nassimramadan <nassim.ramadan@gmail.com>
2021-03-29 12:43:21 +0200

    Pipeline Runner completed 2

--- backend/luna_backend/urls.py
+++ backend/luna_backend/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('backend/admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
