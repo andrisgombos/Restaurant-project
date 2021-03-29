c4c477b25d9d9ea890e429f22e2944d655e1f619
Mario Marchiorello <mario.marchiorello@gmail.com>
2021-03-29 14:55:03 +0200

    added more dependencies

--- frontend/luna_project/src/App.js
+++ frontend/luna_project/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import profilePage from './components/user';
 
 
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Switch>
         {/* <Route exact path='/verification' component={Verification} />
         <Route exact path='/gratulations' component={Congratulations}/> */}
-        <Route exact path='/my-profile' component={ProfilePage} />
+        <Route exact path='/my-profile' component={profilePage} />
 
 
       </Switch>
---
+++ frontend/luna_project/src/components/user/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function profilePage() {
+    return (
+        <>
+        <TitleImage>
+            <img src={Title} alt="unsername-titleimage"/>
+        </TitleImage>
+        <Background>
+            <UserMenuCard>
+                
+            </UserMenuCard>
+        </Background>
+        </>
+    )
+}
