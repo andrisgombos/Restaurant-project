cdb78b4d51daf1fa4d7f9a13c6c78c215ae02dd0
Mario Marchiorello <mario.marchiorello@gmail.com>
2021-03-30 16:07:05 +0200

    after first pull

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import Email from "./components/Authentication/Email";
 import { Register } from "./components/Authentication/Registration";
 import Verification from "./components/Authentication/Verification";
 import SignIn from "./components/Authentication/Signin"
-import Header from "./components/header";
+import Home from "./components/home";
 import ProfilePage from './components/userPage';
 import MyProfilePage from "./components/userPage/userMePage";
 //import {withAuth} from './components/HOC'
@@ -15,7 +15,7 @@ const App = () => {
       <Switch>
         <Route exact path='/registration' component={Register}/>
         <Route exact path='/sign-in' component={SignIn}/>
-        <Route exact path='/home' component={Header}/>
+        <Route exact path='/home' component={Home}/>
         <Route exact path='/user' component={ProfilePage}/>
         {/*next line is just to test pages */}
         <Route exact path='/user-self' component={MyProfilePage}/>
