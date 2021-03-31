afe732606c2eec224d08fa164ff5502ee3dc033e
Andras Gombos <andrisgombos@gmail.com>
2021-03-31 14:48:00 +0200

    registration update

--- frontend/src/components/Authentication/Registration.js
+++ frontend/src/components/Authentication/Registration.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 import Footer from "../footer";
 import Header from "../header";
+import React, {useState} from 'react';
+
 
 // eslint-disable-next-line
 const RegisterCont = styled.div`
@@ -45,16 +47,38 @@ export const Button = styled.button`
     margin-top: 3em;
 `
 
+
+
+
 export const Register = () => {
 
+    const [regEmail,setEmail] = useState("");
 
+    const regHandler = (e) => {
+        e.preventDefault();
+        const credentials = {
+            email: regEmail,
+        }
+        const url = "TBD";
+        const regConfig =  {
+            method: "POST",
+            body: JSON.stringify(credentials),
+            headers: { 'Content-Type': 'application/json'}       
+        };
+        fetch(url, regConfig)
+        .then(result => {return result.json})
+        .then(data => {
+            console.log(data);
+            // move to next page
+        })
+    }
 
     return  <>
                 <Header/>
                 <RegisterCont>
                     <RegisterMsg>Registration</RegisterMsg>
-                    <Inputfield name='email' type='text' placeholder='E-mail address'></Inputfield>
-                    <Button type='submit'>Register</Button>
+                    <Inputfield name='email' type='text' placeholder='E-mail address' value={regEmail} onChange={(e)=>setEmail(e.target.value)}></Inputfield>
+                    <Button type='submit' onClick={regHandler}>Register</Button>
                 </RegisterCont>
                 <Footer/>
             </>
--- frontend/src/components/Authentication/Signin.js
+++ frontend/src/components/Authentication/Signin.js
@@ -4,6 +4,7 @@ import Header from "../header";
 import { RegisterMsg } from "./Registration"
 import { Inputfield } from "./Registration"
 import { Button } from "./Registration"
+import React, { useState } from 'react'
 
 const SignInCont = styled.div`
     background-color: #F2F2F2;
@@ -17,12 +18,15 @@ const SignInCont = styled.div`
 
 const SignIn = () => {
 
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('');
+
     return  <>
                 <Header/>
                 <SignInCont>
                     <RegisterMsg>Login</RegisterMsg>
-                    <Inputfield name='user' type='text' placeholder='Username'></Inputfield>
-                    <Inputfield name='password' type='password' placeholder='Password'></Inputfield>
+                    <Inputfield name='user' type='text' placeholder='Username' value={user} onChange={(e)=>setUser(e.target.value)}></Inputfield>
+                    <Inputfield name='password' type='password' placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)}></Inputfield>
                     <Button type='submit'>Login</Button>
                 </SignInCont>
                 <Footer/>
