76b9d77eca974e85779f423869f3bcca8c2bc381
Mario Marchiorello <mario.marchiorello@gmail.com>
2021-03-31 22:04:15 +0200

    added styling to Users Comments

--- frontend/src/components/userPage/userCenter/commentsByUser/index.jsx
+++ frontend/src/components/userPage/userCenter/commentsByUser/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Main } from '../reviewBox/style'
+
+import { Main, StarRatingInBox } from '../reviewsByUser/style'
 
 export default function CommentByUser() {
     return (
         <>
             <Main>
-                <h1>Comments</h1>
-
+            <h1>Comments</h1>
+            <span><p>Restaurantname</p><p>Date of comment</p></span>
+            <StarRatingInBox />
+            <div>Discription</div>
             </Main>
             
         </>
--- frontend/src/components/userPage/userCenter/index.jsx
+++ frontend/src/components/userPage/userCenter/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import CommentByUser from './commentsByUser'
 // import EditProfile from './editUser'
 //import UserReviews from './reviewsByUser'
 import { Container } from './style'
@@ -11,7 +12,8 @@ export default function CenterContainer() {
         {/* depending on which user card button is clicked, render a component here --> change profile, view Reviews,comments restaurants*/}
             {/* <EditProfile /> */}
             {/* <UserReviews /> */}
-            <UsersRestaurants />
+            {/* <UsersRestaurants /> */}
+        <CommentByUser />
         </Container>    
         </>
     )
