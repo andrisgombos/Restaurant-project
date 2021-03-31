7d97d47a5f2d20113dddaed248fa993e05a208c2
Andras Gombos <andrisgombos@gmail.com>
2021-03-31 16:21:52 +0200

    local changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 //import Email from "./components/Authentication/Email";
-import { Register } from "./components/Authentication/Registration";
+import { Register } from "./components/Authentication/registration/index";
 //import Verification from "./components/Authentication/Verification";
-import SignIn from "./components/Authentication/Signin"
+import SignIn from "./components/Authentication/signin/index"
 import Home from "./components/home";
 import ProfilePage from './components/userPage';
 import MyProfilePage from "./components/userPage/userMePage";
 import Restaurants from "./components/restaurants/index"
-import Reviews from "./components/restaurants/reviews";
 //import {withAuth} from './components/HOC'
 
 const App = () => {
@@ -22,7 +21,6 @@ const App = () => {
         {/*next line is just to test pages */}
         <Route exact path='/user-self' component={MyProfilePage}/>
         <Route exact path='/restaurants' component={Restaurants}/>
-        <Route exact path='/reviews' component={Reviews}/>
 
         {/*<Route exact path='/user-self' component={withAuth(MyProfilePage)}/>*/}
       </Switch>
