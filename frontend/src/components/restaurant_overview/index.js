3b473d56d1d97845b02bd8876d286a049d580d6c
Andras Gombos <andrisgombos@gmail.com>
2021-04-01 23:10:41 +0200

    added restaurant overview details

--- frontend/src/components/restaurant_overview/index.js
+++ frontend/src/components/restaurant_overview/index.js
@@ -2,14 +2,24 @@ import Footer from "../footer/index"
 import Header from "../header/index"
 import React, { useState, useEffect } from 'react';
 import {Mainsection} from "../../globalStyle/globalStyle"
+import {UpperContainer, RestInUpperCont} from "./style"
+import StarRating from "../starrating/index"
 
 const RestaurantOverview = () => {
 
     return  <>
         <Header/>
-        <Mainsection>
-
-        </Mainsection>
+            <UpperContainer>
+                <RestInUpperCont>
+                    <div>
+                        <h1>Willy Wonka's Chocolate Factory</h1>
+                        <h2>Adress</h2>
+                        <StarRating/>
+                    </div>
+                </RestInUpperCont>
+            </UpperContainer>
+            <Mainsection>
+            </Mainsection>
         <Footer/>
         </>
     
