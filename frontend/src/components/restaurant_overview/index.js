02ab643c4b381b3003f6276a9fa6a384186cee46
Andras Gombos <andrisgombos@gmail.com>
2021-04-01 20:30:43 +0200

    rest overview

---
+++ frontend/src/components/restaurant_overview/index.js
@@ -0,0 +1,17 @@
+import Footer from "../footer/index"
+import Header from "../header/index"
+import React, { useState, useEffect } from 'react';
+import {Mainsection} from "../../globalStyle/globalStyle"
+
+const RestaurantOverview = () => {
+
+    return  <>
+        <Header/>
+        <Mainsection>
+
+        </Mainsection>
+        <Footer/>
+        </>
+    
+}
+export default RestaurantOverview
