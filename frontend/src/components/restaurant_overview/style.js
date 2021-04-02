0a877e44348a304fbda38d70617aaee3efcdca18
Andras Gombos <andrisgombos@gmail.com>
2021-04-02 11:46:31 +0200

    rest overview design additions

--- frontend/src/components/restaurant_overview/style.js
+++ frontend/src/components/restaurant_overview/style.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 import img from "../../assets/pictures/rest1.jpg"
+import StarRating from "../starrating/index"
+import { Main } from "../../components/userPage/userCenter/reviewsByUser/style"
+import { OrangeButton } from "../../globalStyle/globalStyle"
 
 
 export const UpperContainer = styled.div`
@@ -23,14 +26,65 @@ export const RestInUpperCont = styled.div`
         }
 
         h1 {
-            font-size: 1.5rem;
-            margin-bottom: 0.7em;
+            font-size: 1.0rem;
             font-weight: bold;
             margin-left: 2em;
         }
         h2 {
-            font-size: 1.2rem;
+            font-size: 0.9rem;
             font-weight: 200;
             margin-left: 2.5em;
         }
+`
+export const Left = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 65%;
+    padding: 0px;
+    margin: 0px;
+    padding-left: 0px;
+    border: 1.5px solid #e5e5e5;
+
+`
+export const LeftInnerWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    border: 1.5px solid #e5e5e5;
+    padding-left: 0px;
+    width: 100%;
+
+
+
+        img {
+            width: 100px;
+            height: 100px;
+        }
+
+`
+export const RestStarRating = styled(StarRating)`
+    width: 80px;
+    height: 80px;
     `
+
+export const Right = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const RightWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+
+
+`
+export const MainWrapper = styled(Main)`
+    display: flex;
+    flex-direction: row;
+    align-content: space-around;
+`
+export const RestButton = styled(OrangeButton)`
+    width: 8rem;
+    height: 2rem;
+    font-size: 0.8em;
+    text-transform: uppercase;
+`
